29a2ba82cc3f95d11a4a6066f0bcd0b69ca7d910
alozano <manete10@gmail.com>
2020-12-04 07:26:10 +0100

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,6 @@ module.exports = {
         filename: 'main.[contenthash].js',
         path: path.resolve(__dirname, 'dist/')
     },
-
     module: {
         rules: [{
                 test: /\.css$/,
